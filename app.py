457752a321ee02f15e4122a0aa22b832a0398bd5
AneeshRijhwani25 <7851950798aneesh@gmail.com>
2024-08-25 17:40:01 +0530

    first

--- app.py
+++ app.py
@@ -1,63 +1,9 @@
-# from flask import Flask, request, jsonify
-# import pickle
-# import os
-
-# app = Flask(__name__)
-
-# # Load the models
-# models = {
-#     'NB': 'models/NB_model.pkl',
-#     'DT': 'models/DT_model.pkl',
-#     'LR': 'models/LR_model.pkl',
-#     'RF': 'models/RF_model.pkl',
-#     'Adaboost': 'models/Adaboost_model.pkl',
-#     'BGc': 'models/Bgc_model.pkl',
-#     'ETC': 'models/ETC_model.pkl',
-#     'GBDT': 'models/GBDT_model.pkl',
-#     'XGB': 'models/xgb_model.pkl'
-# }
-
-# def load_model(model_name):
-#     if model_name in models:
-#         with open(models[model_name], 'rb') as file:
-#             return pickle.load(file)
-#     else:
-#         return None
-
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#     data = request.get_json(force=True)
-    
-#     email_text = data.get('email_text')
-#     classifier = data.get('classifier')
-#     print(email_text)
-#     if not email_text or not classifier:
-#         return jsonify({"error": "Please provide both 'email_text' and 'classifier'"}), 400
-    
-#     model = load_model(classifier)
-    
-#     if model is None:
-#         return jsonify({"error": "Invalid classifier name"}), 400
-    
-
-#     with open('models/vectorizer.pkl', 'rb') as file:
-#         vectorizer = pickle.load(file)
-    
-#     processed_text = vectorizer.transform([email_text])
-    
-#     prediction = model.predict(processed_text)
-    
-#     result = 'spam' if prediction[0] == 'spam' else 'not spam'
-    
-#     return jsonify({"prediction": result})
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
 
 from flask import Flask, request, jsonify
 import pickle
-
+from flask_cors import CORS 
 app = Flask(__name__)
+CORS(app)
 
 # Load the models
 model_files = {
